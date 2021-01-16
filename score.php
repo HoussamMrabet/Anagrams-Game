3210b2969ff10c2713491096189ec7308b61eaef
HoussamMrabet <dodslim100@gmail.com>
2021-01-16 01:35:11 +0100

    .

--- score.php
+++ score.php
@@ -9,53 +9,62 @@
         <ul>
             <li>
                 <div class="ranknbr">1</div>
-                <div class="username">usernamexxxxxxx</div>
+                <div class="username">username</div>
                 <div class="score">70</div>
             </li>
+            <hr>
             <li>
                 <div class="ranknbr">2</div>
                 <div class="username">username</div>
                 <div class="score">70</div>
             </li>
+            <hr>
             <li>
                 <div class="ranknbr">3</div>
                 <div class="username">username</div>
                 <div class="score">70</div>
             </li>
+            <hr>
             <li>
                 <div class="ranknbr">4</div>
                 <div class="username">username</div>
                 <div class="score">70</div>
             </li>
+            <hr>
             <li>
                 <div class="ranknbr">5</div>
                 <div class="username">username</div>
                 <div class="score">70</div>
             </li>
+            <hr>
             <li>
                 <div class="ranknbr">6</div>
                 <div class="username">username</div>
                 <div class="score">70</div>
             </li>
+            <hr>
             <li>
                 <div class="ranknbr">7</div>
                 <div class="username">username</div>
                 <div class="score">70</div>
             </li>
+            <hr>
             <li>
                 <div class="ranknbr">8</div>
                 <div class="username">username</div>
-                <div class="score">7000</div>
+                <div class="score">70</div>
             </li>
+            <hr>
             <li>
                 <div class="ranknbr">9</div>
                 <div class="username">username</div>
-                <div class="score">7000</div>
+                <div class="score">70</div>
             </li>
+            <hr>
             <li>
                 <div class="ranknbr">10</div>
                 <div class="username">username</div>
-                <div class="score">7000</div>
+                <div class="score">70</div>
             </li>
         </ul>
     </div>
