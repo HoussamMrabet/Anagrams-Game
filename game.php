7831878c0d921328933545f4afbafe253ee225a3
HoussamMrabet <dodslim100@gmail.com>
2021-01-18 10:03:36 +0100

    setting the timer and generating the letters

--- game.php
+++ game.php
@@ -5,38 +5,38 @@
 <link rel="stylesheet" href="layout/css/game.css?v=<?php echo time(); ?>">
 
 <h1>ANAGRAMS</h1>
-<div class="letters-gen">
+<div class="letters-gen active">
     <ul>
-        <li class="tile0">A</li>
-        <li class="tile1">B</li>
-        <li class="tile2">C</li>
-        <li class="tile3">D</li>
-        <li class="tile4">E</li>
-        <li class="tile5">F</li>
-        <li class="tile6">G</li>
-        <li class="tile7">H</li>
+        <li class="tile"></li>
+        <li class="tile"></li>
+        <li class="tile"></li>
+        <li class="tile"></li>
+        <li class="tile"></li>
+        <li class="tile"></li>
+        <li class="tile"></li>
+        <li class="tile"></li>
     </ul>
 </div>
 
 <button class="start">START</button>
-<div class="word-guess">
+<div class="word-guess active">
     <ul>
-        <li class="spot0">W</li>
-        <li class="spot1">O</li>
-        <li class="spot2">R</li>
-        <li class="spot3">D</li>
-        <li class="spot4">__</li>
-        <li class="spot5">__</li>
-        <li class="spot6">__</li>
-        <li class="spot7">__</li>
+        <li class="spot">__</li>
+        <li class="spot">__</li>
+        <li class="spot">__</li>
+        <li class="spot">__</li>
+        <li class="spot">__</li>
+        <li class="spot">__</li>
+        <li class="spot">__</li>
+        <li class="spot">__</li>
     </ul>
 </div>
 
 <div class="scores">
     <h2 class="score">Score : 0</h2>
-    <h2 class="high-score">High Score : 78</h2>
-    <p class="quote">Ready !!</p>
-    <p class="timer">01 : 00</p>
+    <h2 class="high-score">High Score : 0</h2>
+    <p class="quote">Start !!</p>
+    <p class="timer"></p>
 </div>
 
 <?php
---
+++ layout/app/app.js
@@ -0,0 +1,110 @@
+// Variables
+const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'G', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const consonants = ['B', 'C', 'D', 'F', 'G', 'H', 'G', 'K', 'L', 'M', 'N','P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z'];
+const vowels = ['A','E','I','O', 'U', 'Y'];
+const start = document.querySelector('.start');
+const tiles = document.querySelectorAll('.tile');
+const letterGen = document.querySelector('.letters-gen');
+const spots = document.querySelectorAll('.spot');
+const wordInput = document.querySelector('.word-guess');
+const score = document.querySelector('.score');
+const highScore = document.querySelector('.high-score');
+const quote = document.querySelector('.quote');
+const timer = document.querySelector('.timer');
+let lettersGen = [];
+let words = [];
+let originalTime = 60;
+let currentTime = originalTime;
+let timerInterval = currentTime;
+let timesUp = false;
+
+
+// Setting the Words List
+let request = new XMLHttpRequest();
+request.open('GET','words-list.txt',false);
+request.send();
+words = request.responseText.split('\n');
+
+// Events
+start.addEventListener('click', startGame);
+
+// Functions
+
+function startGame() {
+    gen();
+    start.classList.toggle('active');
+    letterGen.classList.toggle('active');
+    wordInput.classList.toggle('active');
+    counter();
+}
+
+function gen() {
+
+    for (let i = 0; i < 8; i++){
+        lettersGen.pop();
+    }
+
+    for (let i = 0; i < 3; i++){
+        lettersGen.push(consonants[Math.round(Math.random()*19)]);
+    }
+
+    for (let i = 0; i < 3; i++){
+        lettersGen.push(vowels[Math.round(Math.random()*5)]);
+    }
+
+    for (let i = 0; i < 2; i++){
+        lettersGen.push(letters[Math.round(Math.random()*25)]);
+    }
+
+    tilesFill();
+}
+
+
+function tilesFill() {
+    tiles.forEach((tile,index) => {
+        tile.innerHTML = lettersGen[index];
+    });
+}
+
+function counter() {
+    
+    timerInterval = setInterval(function() {
+        if (currentTime != 0) {
+            currentTime--;
+            if (currentTime < 6) {
+                timer.style.color = 'red';
+            }
+            if (currentTime < 1) {
+                timer.style.color = 'white';
+                quote.innerHTML = "Time's up!";
+            }
+            render(timer, currentTime);
+        } else {
+            timesUp = true;
+            start.classList.toggle('active');
+            letterGen.classList.toggle('active');
+            wordInput.classList.toggle('active');
+            quote.innerHTML = "Start !!";
+            render(timer, originalTime);
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+
+}
+
+function getTimeString(seconds) {
+    let ms = seconds / 60;
+    let s = Math.floor(seconds%60);
+    let m = Math.floor(ms%60);
+    let second = s % 10 == s ? "0" + s : s;
+    let minute = m % 10 == m ? "0" + m : m;
+    return minute + ":" + second;
+}
+
+function render(item, seconds) {
+    item.innerHTML = getTimeString(seconds);
+}
+
+render(timer, currentTime);
+
+
